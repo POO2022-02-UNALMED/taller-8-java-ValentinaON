26c75c66e211e313b2c0787cd172251aef83b81b
ValentinaON <111089153+ValentinaON@users.noreply.github.com>
2022-10-16 17:05:09 -0500

    Cambio en el método to String
    Se corrige el método toString de Jugador y Portero implementando el super de Futbolista correctamente

--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -28,7 +28,7 @@ public class Jugador extends Futbolista{
 	
 	@Override
 	public String toString() {
-		return "El futbolista "+getNombre()+" tiene "+getEdad()+", y juega de "+getPosicion()+" con el dorsal "+dorsal+". Ha marcado "+golesMarcados;
+		return super.toString()+" con el dorsal "+dorsal+". Ha marcado "+golesMarcados;
 	}
 
 	public short getGolesMarcados() {
--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -22,7 +22,7 @@ public class Portero extends Futbolista {
 	
 	@Override
 	public String toString() {
-		return "El futbolista "+getNombre()+" tiene "+getEdad()+", y juega de "+getPosicion()+" con el dorsal "+dorsal+". Le han marcado "+golesRecibidos;
+		return super.toString()+" con el dorsal "+dorsal+". Le han marcado "+golesRecibidos;
 	}
 
 	public short getGolesRecibidos() {
